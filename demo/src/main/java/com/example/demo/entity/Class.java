f8e4f555d68d463d7fecbe750c62aa4cae0fc05f
ttuan9909 <ttanhtuan2003@gmail.com>
2025-11-12 20:49:18 +0700

    new

--- demo/src/main/java/com/example/demo/entity/Class.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entity;
-
-public class Class {
-    private String name;
-    public Class(String name) {
-        this.name = name;
-    }
-}
--- demo/src/main/java/com/example/demo/entity/Student.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entity;
-
-public class Student {
-    private int id;
-    private String name;
-    private int age;
-    public Student(int id, String name, int age) {}
-}
